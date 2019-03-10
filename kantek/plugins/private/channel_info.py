6ee7c431a21f31b13f426456cb37b3e9cd0dd0b3
SitiSchu <admin@sitischu.com>
2019-03-11 00:46:26 +0100

    Update info command with more information
    Signed-off-by: SitiSchu <admin@sitischu.com>

--- kantek/plugins/private/channel_info.py
+++ kantek/plugins/private/channel_info.py
@@ -1,4 +1,6 @@
 """Plugin to get information about a channel."""
+import logging
+
 from telethon import events
 from telethon.events import NewMessage
 from telethon.tl.types import Channel
@@ -7,18 +9,36 @@ from config import cmd_prefix
 
 __version__ = '0.1.0'
 
+tlog = logging.getLogger('kantek-channel-log')
+
 
 @events.register(events.NewMessage(outgoing=True, pattern=f'{cmd_prefix}info'))
 async def info(event: NewMessage.Event) -> None:
-    """Show the id of the group.
+    """Show the information about a group or channel.
 
     Args:
-        event: The event with the command
+        event: The event of the command
 
     Returns: None
 
     """
     chat: Channel = event.chat
 
-    await event.respond(f'{chat.title} `{chat.id}`',
-                        reply_to=event.reply_to_msg_id)
+    _info = {
+        'title': chat.title,
+        'chat_id': event.chat_id,
+        'access_hash': chat.access_hash,
+        'creator': chat.creator,
+        'broadcast': chat.broadcast,
+        'megagroup': chat.megagroup,
+        'min': chat.min,
+        'username': chat.username,
+        'verified': chat.verified,
+        'version': chat.version,
+    }
+    info_msg = [f'info for {chat.title}:']
+    info_msg += [f'**{k}:**\n  `{v}`' for k, v in _info.items() if v is not None]
+
+    await event.respond('\n'.join(info_msg),
+                        reply_to=(event.reply_to_msg_id or event.message.id))
+    tlog.info(f'Ran `info` in `chat.title`')
