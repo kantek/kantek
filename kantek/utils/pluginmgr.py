319e84a7dd1f1237dd16b9f014e0aaec84ec66a3
SitiSchu <admin@sitischu.com>
2019-03-14 23:44:53 +0100

    Use dataclasses in pluginmgr
    Signed-off-by: SitiSchu <admin@sitischu.com>

--- kantek/utils/pluginmgr.py
+++ kantek/utils/pluginmgr.py
@@ -2,6 +2,7 @@
 import ast
 import importlib.util
 import os
+from dataclasses import dataclass
 from importlib._bootstrap import ModuleSpec
 from importlib._bootstrap_external import SourceFileLoader
 from logging import Logger
@@ -12,16 +13,49 @@ from telethon import TelegramClient
 
 logger: Logger = logzero.logger
 
+__version__ = '0.1.0'
+
+
+@dataclass
+class Callback:
+    name: str
+    callback: Callable
+
+
+@dataclass
+class Plugin:
+    """A Plugin with callbacks.
+
+    Attributes:
+        name: Plugin name without path
+        callbacks: List of callbacks the plugin has
+        full_path: Absolute path to the plugin
+        plugin_path: Plugin folder the plugin lies in
+        path: Plugin Path relative to the Plugin Folder
+    """
+    name: str
+    callbacks: List[Callback]
+    full_path: str
+    plugin_path: str
+
+    @property
+    def path(self):
+        """Return the plugin path relative to the plugin folder."""
+        return (os.path.relpath(self.full_path, self.plugin_path)
+                .rstrip('.py')
+                # replace the backslash in windows paths
+                .replace('\\', '/'))
+
 
 class PluginManager:
     """Mange loading and unloading of plugins."""
-    active_plugins: List[Tuple[str, List[str], str]] = []
+    active_plugins: List[Plugin] = []
 
     def __init__(self, client: TelegramClient) -> None:
         self.client = client
         self.plugin_path: str = os.path.abspath('./plugins')
 
-    def register_all(self) -> List[Tuple[str, List[str], str]]:
+    def register_all(self) -> List[Plugin]:
         """Get a list of all plugins and register them with the client.
 
         Returns: List of active plugins
@@ -29,12 +63,18 @@ class PluginManager:
         """
         for plugin_name, path in self._get_plugin_list():
             active_commands = []
-            for name, callback in self._get_plugin_callbacks(plugin_name, path):
+            for callback in self._get_plugin_callbacks(plugin_name, path):
                 logger.debug('Registered plugin %s/%s',
-                             self._get_plugin_location(path), name)
-                active_commands.append(name)
-                self.client.add_event_handler(callback)
-            self.active_plugins.append((plugin_name, active_commands, path))
+                             self._get_plugin_location(path), callback.name)
+                active_commands.append(callback)
+                self.client.add_event_handler(callback.callback)
+            self.active_plugins.append(
+                Plugin(plugin_name,
+                       active_commands,
+                       path,
+                       self.plugin_path))
+
+        logger.info(f'Registered {len(self.active_plugins)} plugins.')
         return self.active_plugins
 
     def _get_plugin_location(self, path: str) -> str:
@@ -53,7 +93,7 @@ class PluginManager:
                     plugins.append((name, path))
         return plugins
 
-    def _get_plugin_callbacks(self, name: str, path: str) -> List[Tuple[str, Callable]]:
+    def _get_plugin_callbacks(self, name: str, path: str) -> List[Callback]:
         _module: ModuleSpec = importlib.util.spec_from_file_location(name, path)
         loader: SourceFileLoader = _module.loader
         module = loader.load_module()
@@ -62,5 +102,5 @@ class PluginManager:
             tree = ast.parse(f.read(), filename=path)
             for func in tree.body:
                 if isinstance(func, ast.AsyncFunctionDef) and not func.name.startswith('_'):
-                    callbacks.append((func.name, getattr(module, func.name)))
+                    callbacks.append(Callback(func.name, getattr(module, func.name)))
         return callbacks
