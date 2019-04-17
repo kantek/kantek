0925019553d80cdc60394cd20aab39bf4f021a9a
SitiSchu <admin@sitischu.com>
2019-04-18 01:41:11 +0200

    Add kantek command to get information about the bot
    Signed-off-by: SitiSchu <admin@sitischu.com>

---
+++ kantek/plugins/builtins/kantek.py
@@ -0,0 +1,38 @@
+"""Plugin to get information about kantek."""
+import logging
+import platform
+
+from telethon import events
+from telethon.events import NewMessage
+from telethon.tl.types import Chat
+
+from config import cmd_prefix
+from utils.client import KantekClient
+
+__version__ = '0.1.0'
+
+tlog = logging.getLogger('kantek-channel-log')
+
+
+@events.register(events.NewMessage(outgoing=True, pattern=f'{cmd_prefix}kantek'))
+async def tag(event: NewMessage.Event) -> None:
+    """Show the information about kantek.
+
+    Args:
+        event: The event of the command
+
+    Returns: None
+
+    """
+    chat: Chat = event.chat
+    client: KantekClient = event.client
+    response = ['**kantek** userbot']
+    _info = {
+        'version': client.kantek_version,
+        'telethon version': client.__version__,
+        'python version': platform.python_version(),
+    }
+
+    response += [f'  **{k}:**\n    `{v}`' for k, v in _info.items() if v is not None]
+    await client.respond(event, '\n'.join(response))
+    tlog.info(f'Ran `kantek` in `{chat.title}`')
