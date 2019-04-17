83b0283cb8acd67e28bf37041fdc51d77ca501c1
SitiSchu <admin@sitischu.com>
2019-04-18 00:23:31 +0200

    Add respond method to client
    Signed-off-by: SitiSchu <admin@sitischu.com>

--- kantek/utils/client.py
+++ kantek/utils/client.py
@@ -10,3 +10,9 @@ class KantekClient(TelegramClient):
     """Custom telethon client that has the plugin manager as attribute."""
     plugin_mgr: Optional[PluginManager] = None
     db: ArangoDB = None
+
+    async def respond(self, event, msg, reply=True):
+        if reply:
+            await event.respond(msg, reply_to=(event.reply_to_msg_id or event.message.id))
+        else:
+            await event.respond(msg, reply_to=event.message.id)
