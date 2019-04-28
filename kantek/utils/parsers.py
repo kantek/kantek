eb95157e6ac9b291778dc7a72d493ad814eb0385
SitiSchu <admin@sitischu.com>
2019-04-28 02:37:19 +0200

    Remove uneeded comma splitting
    Signed-off-by: SitiSchu <admin@sitischu.com>

--- kantek/utils/parsers.py
+++ kantek/utils/parsers.py
@@ -5,12 +5,12 @@ from typing import Dict, List, Pattern, Tuple
 NAMED_ATTRIBUTE: Pattern = re.compile(r'(\w+):\s?(\[.+?\]|".+"|\w+)')
 
 
-def parse_arguments(cmd: str) -> Tuple[Dict[str, List[str]], List[str]]:
+def parse_arguments(cmd: str) -> Tuple[Dict[str, str], List[str]]:
     """Parse the syntax for adding tags"""
 
     _named_attrs = re.findall(NAMED_ATTRIBUTE, cmd)
-    named_attrs: Dict[str, List[str]] = {name: re.sub(r'[\[\]\"]', '', value).split(',')
-                                         for name, value in _named_attrs}
+    named_attrs: Dict[str, str] = {name: re.sub(r'[\[\]\"]', '', value)
+                                   for name, value in _named_attrs}
     cmd = re.sub(NAMED_ATTRIBUTE, '', cmd)
     attrs = re.findall(r'\w+', cmd)
     return named_attrs, attrs
