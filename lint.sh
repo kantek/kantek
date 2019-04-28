9bd625b6fcb6842386659ab3883f99881a5005cf
SitiSchu <admin@sitischu.com>
2019-04-28 03:45:09 +0200

    Update lint.sh
    Signed-off-by: SitiSchu <admin@sitischu.com>

--- lint.sh
+++ lint.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-echo "mypy" && mypy --config-file=mypy.ini kantek && echo "flake8" && flake8 --config=.flake8 kantek && echo "pylint" && pylint --rcfile=.pylintrc kantek
+echo "mypy"
+mypy --config-file=mypy.ini kantek
+
+echo -e "\nflake8"
+flake8 --config=.flake8 kantek
+
+echo -e "\npylint"
+pylint --rcfile=.pylintrc kantek
