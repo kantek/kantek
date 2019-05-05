8f3cb8f5d52eb2210f9a18e95796720329d8fe21
SitiSchu <admin@sitischu.com>
2019-05-05 14:43:43 +0200

    Add Collections for autobahn to database
    Signed-off-by: SitiSchu <admin@sitischu.com>

--- kantek/database/arango.py
+++ kantek/database/arango.py
@@ -65,17 +65,84 @@ class Chats(Collection):
             return self.add_chat(chat_id)
 
 
-# class Group(Document):
+class AutobahnBlacklist(Collection):
+    _fields = {
+        'strings': Field([NotNull()]),
+    }
+
+    _validation = {
+        'on_save': True,
+    }
+
+    _properties = {
+        'keyOptions': {
+            'allowUserKeys': False,
+            'type': 'autoincrement',
+            'offset': '1'
+        }
+    }
+
+    def add_string(self, string) -> Optional[Document]:
+        """Add a Chat to the DB or return an existing one.
+
+        Args:
+            chat_id: The id of the chat
+
+        Returns: The chat Document
+
+        """
+        data = {'string': string}
+
+        try:
+            doc = self.createDocument(data)
+            doc.save()
+            return doc
+        except CreationError:
+            return None
+
+
+class AutobahnBioBlacklist(AutobahnBlacklist):
+    """Blacklist with strings in a bio."""
+    hex_type = '0x0'
+
+
+class AutobahnStringBlacklist(AutobahnBlacklist):
+    """Blacklist with strings in a message"""
+    hex_type = '0x1'
+
+
+class AutobahnFilenameBlacklist(AutobahnBlacklist):
+    """Blacklist with strings in a Filename"""
+    hex_type = '0x2'
+
+
+class AutobahnChannelBlacklist(AutobahnBlacklist):
+    """Blacklist with blacklisted channel ids"""
+    hex_type = '0x3'
 
 
 class ArangoDB:
     """Handle creation of all required Documents."""
+
     def __init__(self) -> None:
         self.conn = Connection(arangoURL=config.db_host,
                                username=config.db_username,
                                password=config.db_password)
         self.db = self._get_db(config.db_name)
         self.groups: Chats = self._get_collection('Chats')
+        self.ab_bio_blacklist: AutobahnBioBlacklist = self._get_collection('AutobahnBioBlacklist')
+        self.ab_string_blacklist: AutobahnStringBlacklist = self._get_collection(
+            'AutobahnStringBlacklist')
+        self.ab_filename_blacklist: AutobahnFilenameBlacklist = self._get_collection(
+            'AutobahnFilenameBlacklist')
+        self.ab_channel_blacklist: AutobahnChannelBlacklist = self._get_collection(
+            'AutobahnChannelBlacklist')
+        self.ab_collection_map = {
+            '0x0': self.ab_bio_blacklist,
+            '0x1': self.ab_string_blacklist,
+            '0x2': self.ab_filename_blacklist,
+            '0x3': self.ab_channel_blacklist
+        }
 
     def _get_db(self, db: str) -> Database:
         """Return a database. Create it if it doesn't exist yet.
