6ee5a4146e28e853b446d307fd2d1084ad1260cc
SitiSchu <admin@sitischu.com>
2019-05-19 17:49:52 +0200

    Add biopolizei
    Signed-off-by: SitiSchu <admin@sitischu.com>

--- kantek/plugins/autobahn/polizei.py
+++ kantek/plugins/autobahn/polizei.py
@@ -5,11 +5,12 @@ import logging
 from typing import Dict, List
 
 from telethon import events
-from telethon.events import NewMessage
+from telethon.events import ChatAction, NewMessage
 from telethon.tl.functions.channels import EditBannedRequest
+from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.patched import Message
 from telethon.tl.types import (Channel, ChannelParticipantsAdmins, ChatBannedRights,
-                               MessageEntityTextUrl)
+                               MessageEntityTextUrl, UserFull)
 
 from database.arango import ArangoDB
 from utils import helpers
@@ -34,24 +35,50 @@ async def polizei(event: NewMessage.Event) -> None:
     if polizei_tag == 'exclude':
         return
     ban_type, ban_reason = await _check_message(event)
-
     if ban_type and ban_reason:
-        msg: Message = event.message
-        if chat.creator or chat.admin_rights:
-            await msg.delete()
-        if chat.creator or chat.admin_rights:
-            if bancmd == 'manual':
-                await client(EditBannedRequest(
-                    chat, msg.from_id, ChatBannedRights(
-                        until_date=datetime.datetime(2038, 1, 1),
-                        view_messages=True
-                    )
-                ))
-            elif bancmd is not None:
-                await msg.reply(f'{bancmd} {ban_reason}')
-                await asyncio.sleep(0.25)
-            await msg.delete()
-        await client.gban(msg.from_id, f'Spambot[kv2 {ban_type} 0x{ban_reason.rjust(4, "0")}]')
+        await _banuser(event, chat, event.message.from_id, bancmd, ban_type, ban_reason)
+
+
+@events.register(events.chataction.ChatAction())
+async def biopolizei(event: ChatAction.Event) -> None:
+    """Plugin to ban users with blacklisted strings in their bio."""
+    client: KantekClient = event.client
+    chat: Channel = await event.get_chat()
+    db: ArangoDB = client.db
+    chat_document = db.groups.get_chat(event.chat_id)
+    db_named_tags: Dict = chat_document['named_tags'].getStore()
+    bancmd = db_named_tags.get('gbancmd')
+    db_tags: List = chat_document['tags']
+    polizei_tag = db_named_tags.get('polizei')
+    if polizei_tag == 'exclude':
+        return
+    ban_type, ban_reason = False, False
+    bio_blacklist = db.ab_bio_blacklist.get_all()
+    user: UserFull = await client(GetFullUserRequest(await event.get_input_user()))
+    for string in bio_blacklist:
+        if string in user.about:
+            ban_type, ban_reason = db.ab_bio_blacklist.hex_type, bio_blacklist[string]
+    if ban_type and ban_reason:
+        await _banuser(event, chat, event.user_id, bancmd, ban_type, ban_reason)
+
+
+async def _banuser(event, chat, userid, bancmd, ban_type, ban_reason):
+    client: KantekClient = event.client
+    if chat.creator or chat.admin_rights:
+        await event.delete()
+    if chat.creator or chat.admin_rights:
+        if bancmd == 'manual':
+            await client(EditBannedRequest(
+                chat, userid, ChatBannedRights(
+                    until_date=datetime.datetime(2038, 1, 1),
+                    view_messages=True
+                )
+            ))
+        elif bancmd is not None:
+            await client.respond(event, f'{bancmd} {ban_reason}')
+            await asyncio.sleep(0.25)
+        await event.delete()
+    await client.gban(userid, f'Spambot[kv2 {ban_type} 0x{ban_reason.rjust(4, "0")}]')
 
 
 async def _check_message(event):
