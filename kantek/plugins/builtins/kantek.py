48b8b079b69b2ff22bacbd72cfa3dee8afe2519f
SitiSchu <admin@sitischu.com>
2019-07-12 21:48:35 +0200

    use MDTeX for .kantek, #KV2-34 fixed
    Signed-off-by: SitiSchu <admin@sitischu.com>

--- kantek/plugins/builtins/kantek.py
+++ kantek/plugins/builtins/kantek.py
@@ -5,19 +5,19 @@ import platform
 import telethon
 from telethon import events
 from telethon.events import NewMessage
-from telethon.tl.types import Chat
 
 from config import cmd_prefix
 from utils.client import KantekClient
+from utils.mdtex import MDTeXDocument, Section, Bold, KeyValueItem
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 tlog = logging.getLogger('kantek-channel-log')
 
 
 @events.register(events.NewMessage(outgoing=True, pattern=f'{cmd_prefix}kantek'))
 async def tag(event: NewMessage.Event) -> None:
-    """Show the information about kantek.
+    """Show information about kantek.
 
     Args:
         event: The event of the command
@@ -25,16 +25,10 @@ async def tag(event: NewMessage.Event) -> None:
     Returns: None
 
     """
-    chat: Chat = event.chat
     client: KantekClient = event.client
-    response = ['**kantek** userbot']
-    _info = {
-        'version': client.kantek_version,
-        'telethon version': telethon.__version__,
-        'python version': platform.python_version(),
-        'plugins loaded': len(client.plugin_mgr.active_plugins)
-    }
-
-    response += [f'  **{k}:**\n    `{v}`' for k, v in _info.items() if v is not None]
-    await client.respond(event, '\n'.join(response))
-    tlog.info('Ran `kantek` in `%s`', chat.title)
+    await client.respond(event, MDTeXDocument(
+        Section(f"{Bold('kantek')} userbot",
+                KeyValueItem(Bold('version'), client.kantek_version),
+                KeyValueItem(Bold('telethon version'), telethon.__version__),
+                KeyValueItem(Bold('python version'), platform.python_version()),
+                KeyValueItem(Bold('plugins loaded'), len(client.plugin_mgr.active_plugins)))))
