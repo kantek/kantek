6a75fe293a43507f32ac0f17f55d9c2a4ec7d00c
SitiSchu <admin@sitischu.com>
2019-07-12 22:54:14 +0200

    Add some more Telegram domains
    Signed-off-by: SitiSchu <admin@sitischu.com>

--- kantek/utils/constants.py
+++ kantek/utils/constants.py
@@ -1,1 +1,8 @@
-TELEGRAM_DOMAINS = ['t.me', 'telegram.org', 'telegram.dog', 'telegra.ph', 'tdesktop.com', 'telesco.pe']
+TELEGRAM_DOMAINS = ['t.me',
+                    'telegram.org',
+                    'telegram.dog',
+                    'telegra.ph',
+                    'tdesktop.com',
+                    'telesco.pe',
+                    'graph.org',
+                    'contest.dev']
