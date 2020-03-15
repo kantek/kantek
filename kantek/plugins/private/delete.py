153ca11110a4f1daa2c9b5e8ed738a573550fdd1
SitiSchu <admin@sitischu.com>
2020-03-15 01:06:24 +0100

    Add .delete, #KV2-151 fixed
    Signed-off-by: SitiSchu <admin@sitischu.com>

---
+++ kantek/plugins/private/delete.py
@@ -0,0 +1,31 @@
+"""Plugin to delete a message"""
+import logging
+
+from telethon import events
+from telethon.events import NewMessage
+from telethon.tl.patched import Message
+from telethon.tl.types import Channel
+
+from config import cmd_prefix
+from utils.client import KantekClient
+
+__version__ = '0.1.0'
+
+tlog = logging.getLogger('kantek-channel-log')
+
+
+@events.register(events.NewMessage(outgoing=True, pattern=f'{cmd_prefix}del(ete)?'))
+async def delete(event: NewMessage.Event) -> None:
+    """Delete the replied to message
+
+    Args:
+        event: The event of the command
+
+    Returns: None
+
+    """
+    msg: Message = event.message
+    await msg.delete()
+    if msg.is_reply:
+        reply_msg: Message = await msg.get_reply_message()
+        await reply_msg.delete()
