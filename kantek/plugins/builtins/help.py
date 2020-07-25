3065f1fa60abb9f667ad5c27ba14c2cac7a0c0bc
SitiSchu <admin@sitischu.com>
2020-07-25 06:23:20 +0200

    Add support for subcommands to help, autoformat sections

--- kantek/plugins/builtins/help.py
+++ kantek/plugins/builtins/help.py
@@ -1,8 +1,13 @@
 import inspect
+import re
+from typing import Callable
 
+from utils._config import Config
 from utils.client import KantekClient
-from utils.pluginmgr import k
 from utils.mdtex import *
+from utils.pluginmgr import k
+
+SECTION_PATTERN = re.compile(r'^(?P<name>[\w ]+:)$', re.MULTILINE)
 
 
 @k.command('help')
@@ -13,6 +18,7 @@ async def help_(client: KantekClient, args, kwargs) -> MDTeXDocument:
     It will get more features once the PluginManager gets some upgrades
     """
     cmds = client.plugin_mgr.commands
+    config = Config()
     if not args:
         _cmds = []
         for cmd in cmds.values():
@@ -30,7 +36,36 @@ async def help_(client: KantekClient, args, kwargs) -> MDTeXDocument:
                     cmd = _cmd
                     break
             if cmd is None:
-                return MDTeXDocument(Section('Error', f'Unknown command `{command}`'))
-
+                return MDTeXDocument(Section('Error', f'Unknown command {Code(command)}'))
         cmd_name = cmd.commands[0]
-        return MDTeXDocument(Section(f'Help for {cmd_name}'), inspect.getdoc(cmd.callback))
+        if not subcommands:
+            help_cmd = f'{config.help_prefix}{cmd_name}'
+            description = get_description(cmd.callback, help_cmd)
+            help_msg = MDTeXDocument(Section(f'Help for {help_cmd}'), description)
+
+            if cmd.subcommands:
+                subcommands = Section('Subcommands')
+                for name, sc in cmd.subcommands.items():
+                    sc_description = inspect.getdoc(sc.callback).split('\n')[0]
+                    subcommands.append(KeyValueItem(Italic(Bold(name)), sc_description,
+                                                    colon_styles=(Bold, Italic)))
+                help_msg.append(subcommands)
+            return help_msg
+        elif subcommands:
+            subcommand = cmd.subcommands.get(subcommands[0])
+
+            if subcommand is None:
+                return MDTeXDocument(Section('Error',
+                                             f'Unknown subcommand {Code(subcommands[0])} for command {Code(cmd_name)}'))
+            help_cmd = f'{config.help_prefix}{cmd_name} {subcommand.command}'
+            description = get_description(subcommand.callback, help_cmd)
+
+            help_msg = MDTeXDocument(Section(f'Help for {help_cmd}'), description)
+
+            return help_msg
+
+
+def get_description(callback: Callable, help_cmd: str) -> str:
+    description = inspect.getdoc(callback).format(cmd=help_cmd)
+    description = SECTION_PATTERN.sub(str(Bold(r'\g<name>')), description)
+    return description
