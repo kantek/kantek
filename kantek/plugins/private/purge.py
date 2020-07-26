838d5080800d67881e8d4687dd7235366865942e
SitiSchu <admin@sitischu.com>
2020-07-26 17:43:27 +0200

    Support count for .purge

--- kantek/plugins/private/purge.py
+++ kantek/plugins/private/purge.py
@@ -11,10 +11,13 @@ tlog = logging.getLogger('kantek-channel-log')
 
 
 @k.command('purge')
-async def puge(client: Client, chat: Channel, msg: Message, event) -> None:
+async def puge(client: Client, chat: Channel, msg: Message, args, event) -> None:
     """Purge all messages from the the point the command was sent to the message that was replied to.
 
     This command is unavailable in private for the moment since private message ids are shared across PMs
+
+    Arguments:
+        `count`: Delete `count` messages
 
     Examples:
         {cmd}
@@ -23,7 +26,14 @@ async def puge(client: Client, chat: Channel, msg: Message, event) -> None:
     if event.is_private:
         return
     if not msg.is_reply:
-        return
+        if args:
+            count = args[0]
+            message_ids = [msg.id]
+            async for m in client.iter_messages(chat, limit=count, offset_id=msg.id):
+                message_ids.append(m.id)
+            await client.delete_messages(chat, message_ids)
+        else:
+            return
     else:
         reply_msg: Message = await msg.get_reply_message()
         await client.delete_messages(chat, list(range(reply_msg.id, msg.id)))
