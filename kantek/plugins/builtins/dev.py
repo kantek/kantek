f9b06efcecf31dcf170e0ede62130197590b4992
SitiSchu <admin@sitischu.com>
2020-07-31 02:28:38 +0200

    Move .arg to .dev args

--- kantek/plugins/builtins/dev.py
+++ kantek/plugins/builtins/dev.py
@@ -1,3 +1,8 @@
+from pprint import pformat
+from typing import List, Dict
+
+from telethon.tl.custom import Message
+
 from utils.client import Client
 from utils.mdtex import *
 from utils.pluginmgr import k, _Command, _Signature
@@ -51,3 +56,36 @@ async def requires(client: Client, args, kwargs) -> MDTeXDocument:
         if _cmd.items:
             result.append(_cmd)
     return MDTeXDocument(result)
+
+@dev.subcommand()
+async def args(msg: Message, args: List, kwargs: Dict) -> MDTeXDocument:
+    """Show the raw output of the argument parser
+
+    Examples:
+        {cmd} arg1 arg2 arg3
+        {cmd} arg1: val1 arg2: "val2.1 val2.2"
+        {cmd} arg: [123, 456] arg2: ["abc", "de f", "xyz"]
+        {cmd} arg: 1..10 arg2: -5..5 arg2: -10..0
+        {cmd} 1e4 2.5e4 125e-5
+        {cmd} 3+3j 4+2i
+        {cmd} keyword: 1 keyword2: 5
+        {cmd} posarg -flag
+        {cmd} posarg -flag 125e-5
+
+    """
+    _args = []
+    for arg in args:
+        _args.append(SubSection(Code(arg),
+                                KeyValueItem('type', Code(type(arg).__name__))))
+    keyword_args = []
+    for key, value in kwargs.items():
+        keyword_args.append(SubSection(Code(key),
+                                       KeyValueItem('value', Code(value)),
+                                       KeyValueItem('type', Code(type(value).__name__))))
+    return MDTeXDocument(
+        Section('Args', *_args),
+        Section('Keyword Args', *keyword_args),
+        Section('Raw',
+                KeyValueItem('args', Pre(pformat(args, width=30))),
+                KeyValueItem('keyword_args', Pre(pformat(kwargs, width=30))))
+    )
