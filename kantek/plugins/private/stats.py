fa5237350d7e670c19672b94b0772f359a61221d
SitiSchu <admin@sitischu.com>
2020-08-01 04:14:55 +0200

    Relayout .stats, add stats about deleted users and bots

--- kantek/plugins/private/stats.py
+++ kantek/plugins/private/stats.py
@@ -22,8 +22,10 @@ async def stats(client: Client, event: Command) -> MDTeXDocument:  # pylint: dis
     """
     waiting_message = await client.respond(event, 'Collecting stats. This might take a while.')
     start_time = time.time()
-    private_chats = 0
+    users = 0
+    deleted_users = 0
     bots = 0
+    deleted_bots = 0
     groups = 0
     broadcast_channels = 0
     admin_in_groups = 0
@@ -52,9 +54,14 @@ async def stats(client: Client, event: Command) -> MDTeXDocument:  # pylint: dis
                     creator_in_groups += 1
 
         elif isinstance(entity, User):
-            private_chats += 1
             if entity.bot:
                 bots += 1
+                if entity.deleted:
+                    deleted_bots += 1
+            else:
+                users += 1
+                if entity.deleted:
+                    deleted_users += 1
 
         elif isinstance(entity, Chat):
             groups += 1
@@ -68,24 +75,33 @@ async def stats(client: Client, event: Command) -> MDTeXDocument:  # pylint: dis
     stop_time = time.time() - start_time
 
     full_name = await helpers.get_full_name(await client.get_me())
-    response = MDTeXDocument(Section(
-        Bold(f'Stats for {full_name}'),
-        SubSection(
-            KeyValueItem(Bold('Private Chats'), private_chats),
-            KeyValueItem(Bold('Users'), private_chats - bots),
-            KeyValueItem(Bold('Bots'), bots)),
-        KeyValueItem(Bold('Groups'), groups),
-        KeyValueItem(Bold('Channels'), broadcast_channels),
-        SubSection(
-            KeyValueItem(Bold('Admin in Groups'), admin_in_groups),
-            KeyValueItem(Bold('Creator'), creator_in_groups),
-            KeyValueItem(Bold('Admin Rights'), admin_in_groups - creator_in_groups)),
-        SubSection(
-            KeyValueItem(Bold('Admin in Channels'), admin_in_broadcast_channels),
-            KeyValueItem(Bold('Creator'), creator_in_channels),
-            KeyValueItem(Bold('Admin Rights'), admin_in_broadcast_channels - creator_in_channels)),
-        KeyValueItem(Bold('Unread'), unread),
-        KeyValueItem(Bold('Unread Mentions'), unread_mentions)),
+    response = MDTeXDocument(
+        Section(Bold(f'Stats for {full_name}')),
+        Section('Private Chats',
+                KeyValueItem('Total', users + bots),
+                SubSection('Users',
+                           KeyValueItem('Total', users),
+                           KeyValueItem('Active', users - deleted_users),
+                           KeyValueItem('Deleted', deleted_users)),
+                SubSection('Bots',
+                           KeyValueItem('Total', bots),
+                           KeyValueItem('Active', bots - deleted_bots),
+                           KeyValueItem('Deleted', deleted_bots))),
+        Section('Groups',
+                KeyValueItem('Total', groups),
+                SubSection('Admin rights',
+                           KeyValueItem('Total', admin_in_groups),
+                           KeyValueItem('Creator', creator_in_groups),
+                           KeyValueItem('Admin', admin_in_groups - creator_in_groups))),
+        Section('Channels',
+                KeyValueItem('Total', broadcast_channels),
+                SubSection('Admin rights',
+                           KeyValueItem('Total', admin_in_broadcast_channels),
+                           KeyValueItem('Creator', creator_in_channels),
+                           KeyValueItem('Admin', admin_in_broadcast_channels - creator_in_channels))),
+        Section('Messages',
+                KeyValueItem('Unread', unread),
+                KeyValueItem('Unread Mentions', unread_mentions)),
         Italic(f'Took {stop_time:.02f}s'))
 
     await waiting_message.delete()
