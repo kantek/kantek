a88e18fa68c6a93611531d8e7eb61384aac8a38b
SitiSchu <admin@sitischu.com>
2020-08-03 00:11:10 +0200

    Fix error in polizei

--- kantek/plugins/autobahn/automatic/polizei.py
+++ kantek/plugins/autobahn/automatic/polizei.py
@@ -102,7 +102,7 @@ async def join_polizei(event: ChatAction.Event) -> None:
 
 
 async def _banuser(event, chat, userid, bancmd, ban_type, ban_reason):
-    formatted_reason = f'Spambot[kv2 {ban_type} 0x{ban_reason.rjust(4, "0")}]'
+    formatted_reason = f'Spambot[kv2 {ban_type} 0x{str(ban_reason).rjust(4, "0")}]'
     client: Client = event.client
     db: Database = client.db
     chat: Channel = await event.get_chat()
