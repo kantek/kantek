d7f2818b3bd038d596ae4421a5a0d3c1c2543d91
SitiSchu <admin@sitischu.com>
2021-10-01 11:19:30 +0200

    Fix report

--- kantek/plugins/autobahn/gban.py
+++ kantek/plugins/autobahn/gban.py
@@ -102,7 +102,7 @@ async def gban(client: Client, db: Database, tags: Tags, chat: Channel, msg: Mes
         await client.gban(uid, ban_reason, message)
         peer_channel: InputPeerChannel = await event.get_input_chat()
         if not client.config.debug_mode:
-            await client(ReportRequest(peer_channel, [reply_msg.id], InputReportReasonSpam()))
+            await client(ReportRequest(peer_channel, [reply_msg.id], InputReportReasonSpam(), 'Spam'))
         if bancmd == 'manual' or bancmd is None:
             if admin:
                 await client.ban(chat, uid)
