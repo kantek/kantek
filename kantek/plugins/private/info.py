1b70fb3e6dddaa9d2b5099409338a829098f38f8
SitiSchu <admin@sitischu.com>
2021-10-04 02:29:15 +0200

    Replace .info with something more useful

--- kantek/plugins/private/info.py
+++ kantek/plugins/private/info.py
@@ -1,51 +1,60 @@
-import logging
-from typing import Optional
+from typing import Dict, List
 
-from telethon.tl.types import Channel, User
-
-from kantek import Client
+import telethon.utils
 from kantex.md import *
-from kantek import k, Command
-from kantek.utils.tags import Tags
+from telethon.tl.functions.channels import GetParticipantRequest
+from telethon.tl.types import Channel, ChannelParticipantsAdmins, ChannelParticipantCreator
 
-tlog = logging.getLogger('kantek-channel-log')
+from kantek import Client, Database, Config
+from kantek import k, Command
 
 
 @k.command('info')
-async def info(client: Client, tags: Tags, chat: Channel, event: Command) -> Optional[KanTeXDocument]:
-    """Show information about a group or channel.
+async def info(client: Client, args: List, db: Database) -> KanTeXDocument:
+    """"Get info about a chat
+
+    Arguments:
+        `chat`: Chat ID or Username
 
     Examples:
-        {cmd}
+        {cmd} -1001129887931
     """
-    if event.is_private:
-        await event.delete()
-        return
-    chat_info = Section(f'Info for {chat.title}:',
-                        KeyValueItem('title', Code(chat.title)),
-                        KeyValueItem('chat_id', Code(event.chat_id)),
-                        KeyValueItem('broadcast', Code(chat.broadcast)),
-                        KeyValueItem('megagroup', Code(chat.megagroup)),
-                        KeyValueItem('username', Code(chat.username)) if chat.username else None,
-                        )
 
-    bot_accounts = 0
-    total_users = 0
-    deleted_accounts = 0
-    user: User
-    async for user in client.iter_participants(chat):
-        total_users += 1
-        if user.bot:
-            bot_accounts += 1
-        if user.deleted:
-            deleted_accounts += 1
+    chat = args[0]
+    entity: Channel = await client.get_cached_entity(chat)
+    if entity is not None:
+        chat_id = telethon.utils.get_peer_id(entity)
+        title = entity.title
+    else:
+        chat = await db.chats.get(chat)
+        if chat is None:
+            return KanTeXDocument(Section('ValueError', 'chat not found'))
+        chat_id = chat.id
+        title = chat.title
 
-    user_stats = Section('User stats:',
-                         KeyValueItem('total_users', Code(total_users)),
-                         KeyValueItem('bots', Code(bot_accounts)),
-                         KeyValueItem('deleted_accounts', Code(deleted_accounts)))
+    sections = [
+        Section('Chat Info',
+                KeyValueItem('chat_id', Code(chat_id)),
+                KeyValueItem('title', Code(title))),
+    ]
 
-    data = []
-    data += [KeyValueItem(key, Code(value)) for key, value in tags.named_tags.items()]
-    tags = Section('Tags:', *data or [Italic('None')])
-    return KanTeXDocument(chat_info, user_stats, tags)
+    if entity is not None:
+        config = Config()
+        items = []
+        admins_ids = []
+        async for p in client.iter_participants(entity, filter=ChannelParticipantsAdmins):
+            participant = await client(GetParticipantRequest(entity, p))
+            if isinstance(participant.participant, ChannelParticipantCreator):
+                items.append(Code(f'{config.prefix}gban {p.id} "Spamadd[0x0 {chat_id}]"'))
+            elif not p.bot:
+                admins_ids.append(str(p.id))
+
+        items.append(Code(f'{config.prefix}gban {" ".join(admins_ids)} "Spamadd[0x1 {chat_id}]"'))
+
+        sections.append(Section('Admins', *items))
+    # TODO Implement this
+    # data = []
+    # data += [KeyValueItem(key, Code(value)) for key, value in tags.named_tags.items()]
+    # sections.append(Section('Tags:', *data or [Italic('None')]))
+
+    return KanTeXDocument(*sections)
